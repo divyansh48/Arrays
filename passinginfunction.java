d09f1943df33330dd4d1ce95717175c91587e473
divyansh48 <166692812+divyansh48@users.noreply.github.com>
2024-05-20 11:36:07 +0530

    Add files via upload

---
+++ passinginfunction.java
@@ -0,0 +1,22 @@
+package com.divyansh;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class passinginfunction {
+    public static void main(String[] args) {
+
+        int[] num = {1,2,3,4,5};
+        System.out.println(Arrays.toString(num));
+
+        change(num);
+
+        System.out.println(Arrays.toString(num));
+
+    }
+
+    static void change(int[] arr){
+        arr[0] = 99;
+
+    }
+}
